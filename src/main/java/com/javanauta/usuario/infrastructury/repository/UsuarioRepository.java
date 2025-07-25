817dc62ebb12fea5e0327642d72d240044ca7a0e
Crystofer Samuel <demetinocrystofer@gmail.com>
2025-07-24 22:43:01 -0300

    adicionadas classes de security

---
+++ src/main/java/com/javanauta/usuario/infrastructury/repository/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package com.javanauta.usuario.infrastructury.repository;
+
+import com.javanauta.usuario.infrastructury.entity.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+
+}
